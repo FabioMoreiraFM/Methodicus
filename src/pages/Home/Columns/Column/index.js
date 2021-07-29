a891626de573c6e8f3975389bf1d1f737c49f1d8
Fabio Moreira <fabio.moreira@dataa.com.br>
2021-07-29 20:00:41 -0300

    Refatora para melhorar legibilidade do código

--- src/pages/Home/Columns/Column/index.js
+++ src/pages/Home/Columns/Column/index.js
@@ -8,6 +8,40 @@ import OptionsPopover from './ColumnPopovers/OptionsPopover';
 
 class Column extends Component {
     render() {
+        const header = (provided) => (
+            <HeaderContainer>
+                <Title {...provided.dragHandleProps}>
+                    {this.props.column.title}
+                </Title>
+                <Options popover={OptionsPopover} onClick>
+                    <MoreHorizIcon />
+                </Options> 
+            </HeaderContainer>
+        )
+        
+        const taskList = (provided) => (
+            <Droppable droppableId={this.props.column.id}>
+                {
+                    (provided, snapshot) => (
+                        <TaskList 
+                            ref={provided.innerRef}
+                            {...provided.droppableProps}
+                            isDraggingOver={snapshot.isDraggingOver}
+                        >
+                            {this.props.tasks.map((task, index) => <Task key={task.id} task={task} index={index} /> )}
+                            {provided.placeholder}
+                        </TaskList>
+                    )
+                }
+            </Droppable>
+        )
+
+        const footer = () => (
+            <NewTaskContainer to="" onClick={() => this.props.onClickNewTask(this.props.column.id)}>
+                + Nova tarefa
+            </NewTaskContainer>
+        )
+        
         return (
             <Draggable draggableId={this.props.column.id} index={this.props.index}>
                 {(provided) => (
@@ -15,31 +49,9 @@ class Column extends Component {
                         {...provided.draggableProps}
                         ref={provided.innerRef}
                     >
-                        <HeaderContainer>
-                            <Title {...provided.dragHandleProps}>
-                                {this.props.column.title}
-                            </Title>
-                            <Options popover={OptionsPopover} onClick>
-                                <MoreHorizIcon />
-                            </Options> 
-                        </HeaderContainer>                                               
-                        <Droppable droppableId={this.props.column.id}>
-                            {
-                                (provided, snapshot) => (
-                                    <TaskList 
-                                        ref={provided.innerRef}
-                                        {...provided.droppableProps}
-                                        isDraggingOver={snapshot.isDraggingOver}
-                                    >
-                                        {this.props.tasks.map((task, index) => <Task key={task.id} task={task} index={index} /> )}
-                                        {provided.placeholder}
-                                    </TaskList>
-                                )
-                            }
-                        </Droppable>
-                        <NewTaskContainer to="" onClick={() => this.props.onClickNewTask(this.props.column.id)}>
-                            + Nova tarefa
-                        </NewTaskContainer>
+                        {header(provided)}             
+                        {taskList(provided)}
+                        {footer()}
                     </Container>
                 )}
             </Draggable>
