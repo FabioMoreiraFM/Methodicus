65e76b1a4dbf7e1b033e20e2cb628477767d77f9
Fabio Moreira <fabio.moreira@dataa.com.br>
2021-08-17 19:29:37 -0300

    Corrige bug do pacote react-beautiful-dnd (Remove overflow-y das colunas)

--- src/pages/Home/Columns/Column/Task/style.js
+++ src/pages/Home/Columns/Column/Task/style.js
@@ -2,11 +2,11 @@ import theme from 'config/theme';
 import styled from 'styled-components'
 
 export const Container = styled.div`
-    border: 1px solid var(--light-grey);
+    border: ${props => (props.isDragging ? '1px solid var(--black)' : '1px solid var(--light-grey)')};
     padding: 8px;
     margin-bottom: 8px;
     border-radius: 4px;
-    background-color: ${props => (props.isDragging ? 'white' : 'white')};    
+    background-color: white;    
     word-break: break-all;
     ${theme.breakpoints.up("lg")} {    
         max-width: 15rem;
--- src/pages/Home/Columns/style.js
+++ src/pages/Home/Columns/style.js
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 export const Container = styled.div`
     display: flex;
     flex-grow: 1;
-    overflow-y: auto;
+    
     ${theme.breakpoints.down("xs")} {    
         padding-top: 3rem;
         flex-direction: column;
